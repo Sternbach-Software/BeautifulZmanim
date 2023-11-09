45346c12e9491a57c02c4993e0f63f56c74f4a1f
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-11-09 18:47:26 -0500

    Use androidTarget instead of android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
     targetHierarchy.default()
-    android {
+    androidTarget {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
