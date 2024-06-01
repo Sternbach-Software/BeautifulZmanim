cb585baca253c44a2346d65c01da2e1f93c0986b
shmueldabomb441 <shmuelsternbach613@gmail.com>
2024-06-01 11:37:45 -0400

    Make ZmanimList a column not a grid

--- composeApp/src/commonMain/kotlin/presentation/components/ZmanimList.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/ZmanimList.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
@@ -39,8 +41,8 @@ internal fun ZmanimList(
     onValueBasedSelected: (ZmanCardModel) -> Unit = {},
     onDateBasedSelected: (ZmanCardModel) -> Unit = {}
 ) {
-    LazyVerticalGrid(
-        GridCells.Fixed(if (smallScreen) 2 else 6),
+    LazyColumn(
+//        GridCells.Fixed(if (smallScreen) 2 else 6),
         Modifier.fillMaxWidth()/*, state*/
     ) {
         shaaZmanisValues?.let {
