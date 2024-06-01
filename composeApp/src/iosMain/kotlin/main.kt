3486fa465c87a905ecba1edfe90983fec83dbbb3
shmueldabomb441 <shmuelsternbach613@gmail.com>
2024-06-01 11:37:15 -0400

    Add @Suppress("unused") to MainViewController

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -2,6 +2,7 @@ import androidx.compose.ui.window.ComposeUIViewController
 import sternbach.software.App
 import platform.UIKit.UIViewController
 
+@Suppress("unused")
 fun MainViewController(): UIViewController {
     return ComposeUIViewController { App(true) }
 }
