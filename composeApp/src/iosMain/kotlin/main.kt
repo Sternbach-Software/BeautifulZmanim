d8cf9610b0aac22b979b39260bef89d2c48996f5
shmueldabomb441 <shmuelsternbach613@gmail.com>
2024-06-03 00:29:43 -0400

    Remove @Suppressed(unused)

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -2,7 +2,6 @@ import androidx.compose.ui.window.ComposeUIViewController
 import sternbach.software.App
 import platform.UIKit.UIViewController
 
-@Suppress("unused")
 fun MainViewController(): UIViewController {
     return ComposeUIViewController { App(true) }
 }
